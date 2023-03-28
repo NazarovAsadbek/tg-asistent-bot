f49f6e88b2bd5e883a197ccca8ad606a32cda111
NazarovAsadbek <66946049+NazarovAsadbek@users.noreply.github.com>
2023-03-29 00:16:08 +0500

    [WIP] Добавлена база данных MONGO DB

--- src/app.ts
+++ src/app.ts
@@ -7,9 +7,11 @@ import {Command} from "./commands/command.class";
 import {StartCommand} from "./commands/start.command";
 import {ChooseIntervalCommand} from "./commands/choose-interval.command";
 import {SendMessageCommand} from "./commands/send-message.command";
+import {MongoClient} from 'mongodb'
 
 class Bot {
     bot: Telegraf<IBotContext>;
+    client: MongoClient;
     commands: Command[] = [];
 
     constructor(private readonly configService: IConfigService) {
@@ -17,6 +19,35 @@ class Bot {
         this.bot.use(
             new LocalSession({database: 'sessions.json'}).middleware()
         );
+        this.client = new MongoClient(this.configService.get('MONGO_DB'));
+    }
+
+    async initDataBase() {
+        try {
+            // Connect to the MongoDB cluster
+            await this.client.connect();
+
+            // Make the appropriate DB calls
+            await this.listDatabases(this.client);
+
+            const dbo = this.client.db("tg-session-data");
+            const myobj = { name: "Company Inc", address: "Highway 37" };
+            await dbo.collection("user-request").insertOne(myobj);
+
+            const data = await dbo.collection("user-request").findOne({});
+            console.log(data)
+        } catch (e) {
+            console.error(e);
+        } finally {
+            await this.client.close();
+        }
+    }
+
+    async listDatabases(client: MongoClient) {
+        const databasesList = await client.db().admin().listDatabases();
+
+        console.log("Databases:");
+        databasesList.databases.forEach(db => console.log(` - ${db.name}`));
     }
 
     init() {
@@ -28,6 +59,9 @@ class Bot {
         for (const command of this.commands) {
             command.handle();
         }
+
+        this.initDataBase().catch(console.error);
+
         this.bot.launch();
     }
 }
